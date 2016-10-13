9e51025141ca981356d51ddd7c93337b79683406
taochen <taochen@TC-MacBookpro.local>
2016-10-14 00:08:19 +0800

    确定输入值为有效值时才添加todo事件

--- main.js
+++ main.js
@@ -6,9 +6,19 @@ $('#new-todo').keydown(function (event) {
     event.preventDefault();
     event.stopPropagation();
     var task = $('#new-todo').val();
-    counter++;
-    var $todolist = $('<li class="active"><input type="checkbox" class="toggle"/><p>'+task+'</p><button class="delete"></button></li>');
-    $('#todolist-all').append($todolist);
-    $('#new-todo').val("");
+    // 确定输入值为有效值时才添加todo事件
+    var valid = false;
+    for (var i = 0; i < task.length; i++) {
+      if(task[i] !== " ") {
+        valid = true;
+        break;
+      }
+    }
+    if (valid) {
+      counter++;
+      var $todolist = $('<li class="active"><input type="checkbox" class="toggle"/><p>'+task+'</p><button class="delete"></button></li>');
+      $('#todolist-all').append($todolist);
+      $('#new-todo').val("");
+    }
   }
 })
